d3ae7d103d39b994d7f22ea41e6edf4874502068
Yan Pilon <y.pilon9@gmail.com>
2024-09-07 19:52:52 -0400

    Add project files.

---
+++ Chip8Emulator/Chip8Emulator.cpp
@@ -0,0 +1,14 @@
+// Chip8Emulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
