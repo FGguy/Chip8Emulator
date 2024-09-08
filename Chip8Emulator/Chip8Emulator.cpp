77e1f58e17c15052f3884840e800d88b2b9a9acb
Yan Pilon <y.pilon9@gmail.com>
2024-09-07 20:35:53 -0400

    take filepath argument open and read file.

--- Chip8Emulator/Chip8Emulator.cpp
+++ Chip8Emulator/Chip8Emulator.cpp
@@ -3,10 +3,33 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n";
+    if (argc < 2) {
+        std::cout << "No file path provided";
+        return 1;
+    }
+
+    std::string filePath = argv[1];
+
+    std::ifstream chip8ROM;
+    chip8ROM.open(filePath);
+
+    if (!chip8ROM.is_open()) {
+        std::cerr << "Error: Could not open file " << filePath << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(chip8ROM, line)) {
+        std::cout << line << '\n';
+    }
+    std::cin >> line;
+
+    chip8ROM.close();
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
